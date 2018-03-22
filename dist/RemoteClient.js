d1deebf1e7b06999c002b718c27e5ae17c254c98
Jonathan Dahan <hi@jonathan.is>
2018-03-22 14:34:23 -0400

    new build lol

--- dist/RemoteClient.js
+++ dist/RemoteClient.js
@@ -161,7 +161,7 @@ class AbstractClient {
   }
 
   async immediatelyRetract(patternString, ...fillerValues) {
-    this.assert(patternString, ...fillerValues);
+    this.retract(patternString, ...fillerValues);
     await this.flushChanges();
   }
 
